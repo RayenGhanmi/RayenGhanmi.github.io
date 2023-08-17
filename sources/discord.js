ff0204a342da87ed54bbcfdb7519cb9386949cad
Rayen Ghanmi <ghanmirayen12@gmail.com>
2023-08-17 13:17:33 +0100

    Add files via upload

--- sources/discord.js
+++ sources/discord.js
@@ -1,33 +1,79 @@
-async function sendContact(ev) {
+function loadJSON(callback) {
+  const xhr = new XMLHttpRequest();
+  xhr.overrideMimeType('application/json');
+  xhr.open('GET', 'members.json', true);
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      callback(JSON.parse(xhr.responseText));
+    }
+  };
+  xhr.send(null);
+}
+
+function sendContact(ev) {
   ev.preventDefault();
 
   const senderEmail = document.getElementById('emailInput').value;
   const senderMessage = document.getElementById('messageInput').value;
 
-  const webhookUrl = 'https://discord.com/api/webhooks/1129345264528932934/VzuvV07_tDBoy_LL-_b1lsNewLVtjLUQbg8ZCyrFQOpK7KcQ8agqBPdgZScup5FEPqOg';
+  loadJSON(function (data) {
+    const members = data.members;
 
-  const webhookBody = {
-    content: `Sender: ${senderEmail}\nMessage: ${senderMessage}` // Send the message as plain text
+    function isMember(email) {
+      return members.includes(email);
+    }
+
+    const webhookUrl = 'https://discord.com/api/webhooks/your_webhook_url_here';
+    const mbr = isMember(senderEmail) ? 'Member' : 'Not a member';
+
+    const webhookBody = {
+    embeds: [
+      {
+        title: 'New submission',
+        description: senderMessage,
+        color: 0xf177a6,
+        author: {
+          name: senderEmail,
+          icon_url: 'https://cdn.discordapp.com/embed/avatars/1.png',
+          url: "https://mail.google.com/mail/u/0/?fs=1&to="+senderEmail+"&su=Support&body&tf=cm",
+        },
+        thumbnail: {
+          url: 'https://i.imgur.com/fosgeaa.png',
+        },
+        timestamp: new Date().toISOString(),
+	      footer: {
+	      	text: 'Website',
+	      	icon_url: 'https://i.imgur.com/CVTIrwN.png',
+	      },
+        fields: [
+          {
+            name: 'Membership',
+            value: mbr,
+          },
+        ],
+      }
+    ]
   };
 
   try {
-    const response = await fetch(webhookUrl, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(webhookBody)
-    });
-
-    if (response.ok) {
-      alert("message sent successfully");
-    } else {
-      throw new Error('Failed to send webhook');
-    }
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', webhookUrl, true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4) {
+        if (xhr.status === 200) {
+          alert('Message sent successfully');
+          location.reload();
+        } else {
+          console.error('Failed to send webhook');
+          alert('There was an error! Try again later or contact the developer.');
+        }
+      }
+    };
+    xhr.send(JSON.stringify(webhookBody));
   } catch (error) {
     console.error('An error occurred:', error);
     alert('There was an error! Try again later or contact the developer.');
   }
-
-  location.reload();
+});
 }
