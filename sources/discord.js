09fa4643d0c82fd353e7d623a46d970bea25f291
Rayen Ghanmi <ghanmirayen12@gmail.com>
2023-08-18 21:52:17 +0100

    Add files via upload

--- sources/discord.js
+++ sources/discord.js
@@ -5,21 +5,35 @@ async function sendContact(ev) {
   const senderMessage = document.getElementById('messageInput').value;
 
   const webhookUrl = 'https://discord.com/api/webhooks/1129345264528932934/VzuvV07_tDBoy_LL-_b1lsNewLVtjLUQbg8ZCyrFQOpK7KcQ8agqBPdgZScup5FEPqOg';
+  let score = 'Not Rated';
+  let clr = 0xf177a6;
+  var button = document.getElementById("sendButton");
 
-  if (!true)
-  {
-    mbr = 'Member'
+  var options = document.getElementsByName('options');
+  for (var i = 0; i < options.length; i++) {
+      if (options[i].checked) {
+          var selectedOption = options[i].value;
+          console.log('Selected option:', selectedOption);
+          if (selectedOption === 'option1') {
+              score = 'Pleased';
+              clr = 0x009600;
+          } else if (selectedOption === 'option2') {
+              score = 'Moderate';
+              clr = 0xb3b322;
+          } else if (selectedOption === 'option3') {
+              score = 'Unhappy';
+              clr = 0x911919;
+          }
+          break;
+      }
   }
-  else
-  {
-    mbr = 'Not a member'
-  }
+
   const webhookBody = {
     embeds: [
       {
         title: '**New submission**',
         description: '```'+senderMessage+'```',
-        color: 0xf177a6,
+        color: clr,
         author: {
           name: senderEmail,
           icon_url: 'https://cdn.discordapp.com/embed/avatars/1.png',
@@ -29,10 +43,10 @@ async function sendContact(ev) {
           url: 'https://i.imgur.com/fosgeaa.png',
         },
         timestamp: new Date().toISOString(),
-	      footer: {
-	      	text: 'Website',
-	      	icon_url: 'https://i.imgur.com/CVTIrwN.png',
-	      },
+        footer: {
+          text: 'Website',
+          icon_url: 'https://i.imgur.com/CVTIrwN.png',
+        },
         fields: [
           {
             name: '**Section**',
@@ -41,12 +55,12 @@ async function sendContact(ev) {
           },
           {
             name: '**Membership**',
-            value: '> '+mbr,
+            value: '> Null',
             inline: true
           },
           {
             name: '**Rating**',
-            value: '> Not rated',
+            value: '> '+score,
             inline: true
           },
         ],
@@ -64,15 +78,23 @@ async function sendContact(ev) {
     });
 
     if (response.ok) {
-      alert("message sent successfully");
-      location.reload();
+      /*alert("message sent successfully");*/
+      button.innerHTML = "Sent";
+      button.disabled = true;
+      button.style.backgroundColor = "green";
+      button.classList.add("unhoverable");
     } else {
       throw new Error('Failed to send webhook');
     }
   } catch (error) {
     console.error('An error occurred:', error);
-    alert('There was an error! Try again later or contact the developer.');
+    /*alert('There was an error! Try again later or contact the developer.');*/
+    button.innerHTML = "Error";
+    button.disabled = true;
+    button.style.backgroundColor = "red";
+    button.classList.add("unhoverable");
   }
-
-  location.reload();
+  setTimeout(function() {
+    location.reload();
+}, 2000);
 }
